869a705c4e3642b55fb8d18496121a54e02af8ee
IsaiasJoel <isa.dominguez.mon@gmail.com>
2023-12-02 20:49:09 -0500

    estructra de carpetas y configuración inicial

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    HOST: 'http://localhost:8080',
+    TOKEN_NAME: 'access_token',
+    REINTENTOS:2
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    HOST: '',
+    TOKEN_NAME: 'access_token',
+    REINTENTOS: 2
+};
