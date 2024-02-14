a62532468ba2cdb13bd1e5f521fe7fc081145d8c
IsaiasJoel <isa.dominguez.mon@gmail.com>
2024-02-13 21:29:53 -0500

    casos asignados 13-02-2024

--- src/app/modules/admin/usuario/casos-asignados-por-usuario/casos-asignados-por-usuario.component.ts
+++ src/app/modules/admin/usuario/casos-asignados-por-usuario/casos-asignados-por-usuario.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'casos-asignados-por-usuario',
   templateUrl: './casos-asignados-por-usuario.component.html'
 })
 export class CasosAsignadosPorUsuarioComponent {
+  //=========================================================
+  //Ciclo de vida
+  //=========================================================
+  constructor(
+    private _router: Router
+  ) { }
 
+  //=========================================================
+  //Métodos privados
+  //=========================================================
+
+  //=========================================================
+  //Métodos públicos
+  //=========================================================
+  irAPantallaListar() {
+
+  }
 }
