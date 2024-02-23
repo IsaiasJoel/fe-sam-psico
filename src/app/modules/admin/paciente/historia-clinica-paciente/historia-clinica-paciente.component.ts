9c77842d022ae6c60afbb5784ee6a3a226dd70fc
Isaias Joel Dominguez Montalvan <jdominguez@satch.local>
2024-02-23 14:57:03 -0500

    23-02-2024

--- src/app/modules/admin/paciente/historia-clinica-paciente/historia-clinica-paciente.component.ts
+++ src/app/modules/admin/paciente/historia-clinica-paciente/historia-clinica-paciente.component.ts
@@ -23,6 +23,37 @@ export class HistoriaClinicaPacienteComponent {
           { tipo: 'IMG', nombre: 'Archivo 3', contenido: '' },
           { tipo: 'IMG', nombre: 'Archivo 4', contenido: '' }
         ]
+      },
+      {
+        fechaHora: '20/02/2024 4:00 - 6:00 pm',
+        ambiente: 'Consultorio 1',
+        estadoPago: 'PENDIENTE',
+        notas: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        archivosAdjuntos: [
+          { tipo: 'PDF', nombre: 'Archivo 1', contenido: '' },
+          { tipo: 'PDF', nombre: 'Archivo 2', contenido: '' },
+          { tipo: 'IMG', nombre: 'Archivo 3', contenido: '' },
+          { tipo: 'IMG', nombre: 'Archivo 4', contenido: '' }
+        ]
+      },
+      {
+        fechaHora: '20/02/2024 4:00 - 6:00 pm',
+        ambiente: 'Consultorio 1',
+        estadoPago: 'PAGADO',
+        notas: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        archivosAdjuntos: []
+      },
+      {
+        fechaHora: '20/02/2024 4:00 - 6:00 pm',
+        ambiente: 'Consultorio 1',
+        estadoPago: 'GRATIS',
+        notas: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        archivosAdjuntos: [
+          { tipo: 'PDF', nombre: 'Archivo 1', contenido: '' },
+          { tipo: 'PDF', nombre: 'Archivo 2', contenido: '' },
+          { tipo: 'IMG', nombre: 'Archivo 3', contenido: '' },
+          { tipo: 'IMG', nombre: 'Archivo 4', contenido: '' }
+        ]
       }
     ]
   }
